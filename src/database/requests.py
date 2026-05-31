dc279e55f6c5e0461938980aeaea4f010a82edde
Chris Rios <robotman40@protonmail.com>
2026-05-31 15:34:48 +0000

    Add initial request module for database

---
+++ src/database/requests.py
@@ -0,0 +1,17 @@
+from database.database import session
+from database.models import Account, Request as RequestModel, ContainerRequest
+from sqlmodel import select
+
+def create_request(ucinetid: str, request: ContainerRequest):
+    acc_id = session.exec(select(Account.id).where(Account.email == f"{ucinetid}@uci.edu")).first()
+    # Validate the request is valid
+    if session.exec(select(RequestModel.id).where(RequestModel.id == acc_id)).first() is not None:
+        raise fastapi.HTTPException(status_code=400, detail="You already have a pending request")
+
+    req_len = len(request.request_body.strip())
+    if req_len < 300 or req_len > 1000:
+        raise fastapi.HTTPException(status_code=400, detail="A minimum of 300 characters is required")
+
+    new_request = RequestModel(id=acc_id, request=request.request_body)
+    session.add(new_request)
+    session.commit()
