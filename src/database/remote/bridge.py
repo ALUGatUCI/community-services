0a314eca10aa5f5249ac5fa276f982ce0bfeabcb
Chris Rios <robotman40@protonmail.com>
2026-05-31 07:06:48 +0000

    Implement initial interface for Pocketbase

---
+++ src/database/remote/bridge.py
@@ -0,0 +1,12 @@
+from configuration import read_config_file
+from pocketbase import PocketBase
+
+client = PocketBase(read_config_file("pocketbase_url"))
+
+user_data = client.collection("users").auth_with_password(
+    read_config_file("pocketbase_username"),
+    read_config_file("pocketbase_password"),
+)
+
+if not user_data.is_valid:
+    raise RuntimeError("Invalid credentials")
