fdbc03e0c563a14398d6bac9c7d26d7e628b9901
Chris Rios <robotman40@protonmail.com>
2026-07-15 12:16:24 +0000

    Change prefix for router

--- container-api/src/main.py
+++ container-api/src/main.py
@@ -4,6 +4,6 @@ import uvicorn
 from api import router as api_router
 
 app = fastapi.FastAPI()
-app.include_router(api_router, prefix="/container")
+app.include_router(api_router, prefix="/api")
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
